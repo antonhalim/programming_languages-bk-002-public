da5d559ec473e67f8c532dfc14851fa75f2b49e6
axton21 <axton_n@yahoo.com>
2015-02-05 13:09:12 -0500

    when you juggling things up, love is in the air

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,30 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+    reformedhash = {}
+    languages.each do |program, value|
+      #key = :oo,:functional #value = ruby, clojure
+      value.each do |lang, type|
+        #lang = ruby type = :type
+        type.each do |key, style|
+          # key = type style "interpreted"
+          reformedhash[lang] ||= {}
+          #reformedhash[lang] = :ruby
+          reformedhash[lang][key] ||= nil
+          #reformedhash[lang][key] = type
+          #-------------------------------
+          if reformedhash[lang][key] == nil
+              reformedhash[lang][key] ||= ""
+              reformedhash[lang][key]<<style
+          end
+          reformedhash[lang][:style] ||= []
+          reformedhash[lang][:style]<<program
+
+
+            # binding.pry
+        end
+      end
+    end
+    reformedhash
 end
+
